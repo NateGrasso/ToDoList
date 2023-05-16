66ae4256ae6bd76b8c1f1c7a2c71bbf33df70916
Nathan Grasso <nathangrasso@yahoo.com>
2023-05-16 17:10:02 -0600

    made the plus button on the profile view functional ie: text fields for task title, date picker, and time selection. Also made it so when you click the save button the date picker goes away.

--- ToDoList/Views/MainView.swift
+++ ToDoList/Views/MainView.swift
@@ -11,11 +11,24 @@ struct MainView: View {
     @StateObject var viewModel = MainViewModel()
     
     var body: some View {
-        if viewModel.isSignedIn, viewModel.currentUserId.isEmpty{
-            // signed in
-            ToDoListView()
+        if viewModel.isSignedIn, !viewModel.currentUserId.isEmpty{
+            accountView
         } else{
             LogInView()
+        }
+    }
+    
+    @ViewBuilder
+    var accountView: some View {
+        TabView{
+            ToDoListView(userId: viewModel.currentUserId)
+                .tabItem {
+                    Label("Home", systemImage: "house")
+                }
+            ProfileView()
+                .tabItem{
+                    Label("Profile", systemImage: "person.circle")
+                }
         }
     }
 }
